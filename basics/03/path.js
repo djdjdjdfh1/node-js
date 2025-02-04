d37af039b596125ea56ae24e18eadd0ce3f61eaa
Park Chan <qkrcks8028@gmail.com>
2025-02-04 19:40:51 +0900

    Node 2일차

--- basics/03/path.js
+++ basics/03/path.js
@@ -1,2 +1,13 @@
 // path 모듈 연습하기 ( 결과 비교 파일 : 03\results\path.js)
+const path = require('path');
 
+const fullPath = path.join('some', 'work', 'ex.txt');
+console.log(fullPath);
+
+// 경로만 추출
+const dir = path.dirname(fullPath);
+console.log(dir);
+
+// 파일 이름만 추출
+const fn1 = path.basename(__filename);
+console.log(fn1);
