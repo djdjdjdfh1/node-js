c56393ee3cd801b35408aa8a8af08a46c978df95
Park Chan <qkrcks8028@gmail.com>
2025-02-05 21:05:37 +0900

    몽고DB 연결

--- myContacts/app.js
+++ myContacts/app.js
@@ -1,13 +1,17 @@
 const express =  require('express');
+const dbConnect = require('./config/dbConnect');
+
 const app = express();
+dbConnect();
 
 app.get('/', (req, res) => {
     res.send('Hello, Node!')
 })
 
-app.get('/contacts', (req, res) => {
-    res.send('Contacts Page')
-})
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/contacts', require('./routes/contactRoutes'));
 
 app.listen(3000, () => {
     console.log('서버 실행중');
---
+++ myContacts/routes/contactRoutes.js
@@ -0,0 +1,33 @@
+const express =  require('express');
+const router = express.Router();
+
+router.route('/').get(
+    (req, res) => {
+        res.send('Contacts Page')
+    })
+    .post(
+    (req, res) => {
+        console.log(req.body);
+        const {name, email, phone} = req.body;
+        if (!name || !email || !phone) {
+            return res.send('필수값이 입력되지 않았습니다.')
+        }
+        
+        res.send('Create Contacts')
+    });
+
+router.route('/:id').get(
+    (req, res) => {
+        res.send(`View Contact for ID: ${req.params.id}`)
+    }
+).put(
+    (req, res) => {
+        res.send(`Update Contacts for ID: ${req.params.id}`)
+    }
+).delete(
+    (req, res) => {
+        res.send(`Delete Contacts for ID: ${req.params.id}`)
+    }
+)
+
+module.exports = router;
