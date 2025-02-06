f51cfe09886077dd15680fd4b0f84a733f9c389a
Park Chan <qkrcks8028@gmail.com>
2025-02-06 20:33:34 +0900

    ejs 문법

--- myContacts/controllers/contactController.js
+++ myContacts/controllers/contactController.js
@@ -1,8 +1,11 @@
 const asyncHandler = require('express-async-handler')
+const Contact = require('../models/contactModel')
+
 //  Get all contacts
 
 const getAllContacts = asyncHandler(async(req,res) => {
-    res.send("Contacts Page");
+    const contacts = await Contact.find();
+    res.render('index', { contacts });
 })
 
 const createContact = asyncHandler(async(req, res) => {
@@ -11,6 +14,8 @@ const createContact = asyncHandler(async(req, res) => {
     if (!name || !email || !phone) {
         return res.send('필수값이 입력되지 않았습니다.')
     }
+
+    const contact = await Contact.create({ name, email, phone});
     
     res.send('Create Contacts')
 })
@@ -18,22 +23,40 @@ const createContact = asyncHandler(async(req, res) => {
 // @desc Get contact
 // @route GET /contacts/:id
 const getContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    res.send(contact);
   // 연락처 상세 보기
-  res.status(200).send(`View Contact for ID: ${req.params.id}`);
+//   res.status(200).send(`View Contact for ID: ${req.params.id}`);
 });
 
 // @desc Update contact
 // @route PUT /contacts/:id
 const updateContact = asyncHandler(async (req, res) => {
-  // 연락처 수정하기
-  res.status(200).send(`Update Contact for ID: ${req.params.id}`);
+    const id = req.params.id;
+    const {name, email, phone} = req.body;
+    const contact = await Contact.findById(id);
+    if (!contact) {
+        throw new Error("Contact not found.")
+    }
+
+    contact.name = name;
+    contact.email = email;
+    contact.phone = phone;
+
+    contact.save();
+    res.json(contact);
 });
 
-// @desc Delete contact
-// @route DELETE /contacts/:id
 const deleteContact = asyncHandler(async (req, res) => {
-  // 연락처 삭제하기
-  res.status(200).send(`Delete Contact for ID: ${req.params.id}`);
+    const id = req.params.id;
+
+    const contact = await Contact.findById(id);
+
+    if (!contact) {
+        throw new Error("Contact not found");
+    }
+    await Contact.deleteOne();
+    res.send('Deleted')
 });
 
 
