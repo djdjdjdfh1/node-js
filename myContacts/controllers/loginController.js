aba52c794338909357c057ad0997838bf38d2b7b
Park Chan <qkrcks8028@gmail.com>
2025-02-10 20:20:34 +0900

    연락처 마무리

--- myContacts/controllers/loginController.js
+++ myContacts/controllers/loginController.js
@@ -1,17 +1,47 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+const jwtSecret = process.env.JWT_SECRET;
+const jwt = require('jsonwebtoken');
 
 const getLogin = (req, res) => {
     res.render('home');
 }
 
-const loginUser = asyncHandler((req,res) => {
+const loginUser = asyncHandler(async (req,res) => {
     const {username, password} = req.body;
+    const user = await User.findOne({username});
 
-    if (username === 'admin' && password === '1234') {
-        res.redirect('/contacts')
-    } else {
-        res.send("Login failure");
+    if (!user) {
+        return res.json({message: '일치하는 사용자가 없습니다.'})
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return json({message: '비밀번호가 일치하지 않습니다.'})
+    } 
+
+    const token = jwt.sign({id: user._id}, jwtSecret);
+    res.cookie('token', token, {httpOnly: true});
+    res.redirect('/contacts');
 }) 
 
-module.exports = {getLogin, loginUser};
+// Register page
+const getRegister = (req, res) => {
+    res.render('register');
+}
+
+// Register user
+const registerUser = asyncHandler(async(req, res) => {
+    const { username, password, password2} = req.body;
+    if (password === password2) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create( {username, password: hashedPassword} );
+        res.json({message: "Register Success", user})
+    } else {
+        res.send('Register Failed')
+    }
+})
+
+module.exports = {getLogin, loginUser, getRegister, registerUser};
