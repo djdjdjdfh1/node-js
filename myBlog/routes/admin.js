00577238345702c349b33a3b69b3da0324e58207
Park Chan <qkrcks8028@gmail.com>
2025-02-12 19:19:17 +0900

    blog 기본 기능 완성

--- myBlog/routes/admin.js
+++ myBlog/routes/admin.js
@@ -9,6 +9,21 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const jwtSecret = process.env.JWT_SECRET;
 
+const checkLogin = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        res.redirect('/admin');
+    } else {
+        try {
+            const decoded = jwt.verify(token, jwtSecret);
+            req.userId = decoded.userId;
+            next();
+        } catch (error) {
+            res.redirect('/admin');
+        }
+    }
+}
+
 router.get("/admin", (req, res) => {
     const locals = {
         title: '관리자 페이지'
@@ -45,7 +60,7 @@ router.post('/register', asyncHandler(async(req,res) => {
     // res.json(`user created: ${user}`)
 }))
 
-router.get('/allPosts', asyncHandler(async(req, res) => {
+router.get('/allPosts', checkLogin, asyncHandler(async(req, res) => {
     const locals = {
         title: "Posts",
     }
@@ -58,5 +73,45 @@ router.get('/logout', (req, res) => {
     res.clearCookie('token');
     res.redirect('/');
 })
+
+router.get('/add', checkLogin, asyncHandler(async(req, res) => {
+    const locals = {
+        title: '게시글 작성',
+    }
+    res.render("admin/add", {locals, layout: adminLayout});
+}))
+
+router.post('/add', checkLogin, asyncHandler(async(req, res) => {
+    const {title, body} = req.body;
+    const newPost = new Post({
+        title,
+        body
+    })
+    await Post.create(newPost);
+    res.redirect('/allPosts');
+}))
+
+router.get('/edit/:id', checkLogin, asyncHandler(async(req, res) => {
+    const locals = {
+        title: '게시물 편집'
+    }
+    const data = await Post.findOne({ _id: req.params.id });
+    res.render('admin/edit', {locals, data, layout:adminLayout});
+    
+}))
+
+router.put('/edit/:id', checkLogin, asyncHandler(async(req, res) => {
+    await Post.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        body: req.body.body,
+        createdAt: Date.now()
+    })
+    res.redirect('/allPosts');
+}))
+
+router.delete('/delete/:id', checkLogin, asyncHandler(async(req,res) => {
+    await Post.deleteOne({ _id: req.params.id });
+    res.redirect('/allPosts');
+}));
 
 module.exports = router;
